888c6efe151eb5cefc6dd876481998d8ba4284df
shishirprakash <shishirprakash@live.com>
2017-09-21 01:15:02 +0530

    Create stopJstackcollect.sh

---
+++ stopJstackcollect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for pid in `ps -ef | grep -w "collectJstack.sh daemon" | grep -v -w -e grep | tr -s '  ' ' ' | cut -f 2 -d " "`; do
+  echo Killing process tree for `ps -f --pid $pid | sed 1d`
+  for p in `pstree -p $pid | tr -s '(' '\n' | grep ")" | cut -f 1 -d ")"`; do
+    kill -9 $p > /dev/null 2>&1
+  done
+done
+if [ `ps -ef | grep -w "collectJstack.sh daemon" | grep -v -w -e grep | wc -l` -eq 0 ]; then
+  echo Stopped server diagnostics
+else
+  echo ERROR: Failed to stop jstack script!
+  ps -ef | grep -w "collectJstack.sh daemon" | grep -v -w -e grep
+fi
